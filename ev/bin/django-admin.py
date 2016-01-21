1e26204fe66ec251437f04aa6fffd8d2143d0785
mario maidana <mariomaidana82@gimail.com>
2016-01-21 00:40:28 -0300

    Entorno virtual + Django 1.8  install

---
+++ ev/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/marioub/tecnicas_programacion_final_2015/srp/SistemaRecomendacionProfesores/ev/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
