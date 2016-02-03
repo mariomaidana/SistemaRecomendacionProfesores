d3b3fd07d653cc5d5af06374baa20d7322496b84
mario maidana <mariomaidana82@gimail.com>
2016-02-03 00:28:08 -0300

    Conexion con la Base de Datos Mysql Se configuro el setting para lograr la conexion con la base de datos MySql. conexion con la db exitosa!

--- app/app/settings.py
+++ app/app/settings.py
@@ -73,11 +73,23 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}'''
+
+#Coneccion con la base de datos MySql
+DATABASES = {
+    'default' : {
+    'ENGINE'  : 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+    'NAME'    : 'db_Sistema_Recomendacion_Profesores', # Or path to database file if using sqlite3.
+    'USER'    : 'root', # Not used with sqlite3.
+    'PASSWORD': 'root', # Not used with sqlite3.
+    'HOST'    : 'localhost', # Set to empty string for localhost. Not used with sqlite3.
+    'PORT'    : '3306', # Set to empty string for default. Not used with sqlite3.
     }
 }
 
